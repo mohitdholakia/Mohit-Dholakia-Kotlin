9890249101fb7fd4ad3a1e910cab82b3ec7b77f3
MOHIT <ermohitsoni4@gmail.com>
2021-09-24 09:49:08 +0530

    [fix] - crash fix

--- app/src/main/java/com/md/demo/misc/binding/BindingUtils.kt
+++ app/src/main/java/com/md/demo/misc/binding/BindingUtils.kt
@@ -6,10 +6,13 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 object BindingUtils {
+    @JvmStatic
     @BindingAdapter("image")
-    fun ImageView.setGlideImage(res: String) {
-        Glide.with(this)
+    fun ImageView.setGlideImage(res: String?) {
+        if(res!=null){
+            Glide.with(this)
                 .load(res)
                 .into(this)
+        }
     }
 }
